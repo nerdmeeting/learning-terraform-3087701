75407b3746762710b7dca979346b3053320fd465
Nathan Bills <nathan@nerdmeeting.com>
2023-11-13 18:41:45 -0700

    Change alb listeners format to match LinkedIn Learning instructions.

--- main.tf
+++ main.tf
@@ -64,13 +64,13 @@ module "alb" {
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  listeners = {
-    ex-http = {
+  http_tcp_listeners = [
+    {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
     }
-  }
+  ]
 
   target_groups = [
     {
