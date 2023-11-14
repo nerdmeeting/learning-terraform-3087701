3b66c9f9c4cefb62fece5e7bbc276ecdd47c4fbe
Nathan Bills <nathan@nerdmeeting.com>
2023-11-13 18:39:26 -0700

    Change alb target_groups format to match LinkedIn Learning instructions.

--- main.tf
+++ main.tf
@@ -72,20 +72,20 @@ module "alb" {
     }
   }
 
-  target_groups = {
-    ex-instance = {
+  target_groups = [
+    {
       name_prefix = "blog-"
       protocol    = "HTTP"
       port        = 80
       target_type = "instance"
-      targets = {
+      targets     = {
         my_target = {
           target_id = aws_instance.blog.id
           port      = 80
         }
       }
     }
-  }
+  ]
 
   tags = {
     Environment = "dev"
