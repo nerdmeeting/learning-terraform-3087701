5daacb3aa28bb72a18ac71bc654bed49008843ee
Nathan Bills <nathan@nerdmeeting.com>
2023-11-20 15:09:30 -0700

    Revert "Wrap targets in my_target."
    This reverts commit 7ab4f8e5bcf3a9f7528aad596a0f97209648cc58.

--- main.tf
+++ main.tf
@@ -78,11 +78,9 @@ module "alb" {
       protocol    = "HTTP"
       port        = 80
       target_type = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port      = 80
-        }
+      targets     = {
+        target_id = aws_instance.blog.id
+        port      = 80
       }
     }
   }
