83eacb6cab13c1fd2430afc906c30117849bf003
Nathan Bills <nathan@nerdmeeting.com>
2023-11-20 15:09:30 -0700

    Revert "Move blog_sg block about the alb module to see if that fixes the Terraform Run error."
    This reverts commit 2f7b626ce54dca42ebead238747e6d702496bae6.

--- main.tf
+++ main.tf
@@ -42,20 +42,6 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "blog_sg" {
-  source = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-  name = "blog"
-
-  vpc_id = module.blog_vpc.vpc_id
-
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-}
-
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
@@ -88,4 +74,18 @@ module "alb" {
   tags = {
     Environment = "dev"
   }
+}
+
+module "blog_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "blog"
+
+  vpc_id = module.blog_vpc.vpc_id
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
